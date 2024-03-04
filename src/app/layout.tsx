bdfae77f328facd23d099e553bbc70a5055f4b51
croissain <nhattho03@gmail.com>
2024-03-04 10:16:22 +0700

    add seo thumbnail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   title: "BleBle",
   description: "The First Pure Meme NFT on Blast.",
   openGraph: {
-    images: "/bleble-face.png",
+    images: "/thumbnail.png",
   },
 };
 
